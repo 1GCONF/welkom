e80a983632fb1f10387950516b91959e53a8cf5b
g <igorlee@mail.com>
2023-01-23 11:11:08 +0100

    ...

--- src/main/java/be/vdab/welkom/repositories/LandRepository.java
+++ src/main/java/be/vdab/welkom/repositories/LandRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Component
 public class LandRepository {
     public LandRepository() {
-        System.out.println("Constructor");
+        System.out.println("Constructor...");
     }
 
     public List<Land> findAll() {
